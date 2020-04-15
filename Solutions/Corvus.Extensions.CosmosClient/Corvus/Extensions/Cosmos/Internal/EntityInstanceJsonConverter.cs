ecd91de54e7bc1131254bfaf9fc79da2b7b6e5f1
Mike Larah <mike.larah@endjin.com>
2020-04-15 17:45:57 +0100

    Update json converter methods to support nullable values (as per latest JSON.NET source code)

--- Solutions/Corvus.Extensions.CosmosClient/Corvus/Extensions/Cosmos/Internal/EntityInstanceJsonConverter.cs
+++ Solutions/Corvus.Extensions.CosmosClient/Corvus/Extensions/Cosmos/Internal/EntityInstanceJsonConverter.cs
@@ -22,7 +22,7 @@ namespace Corvus.Extensions.Cosmos.Internal
         }
 
         /// <inheritdoc/>
-        public override object? ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
         {
             if (reader is null)
             {
@@ -52,7 +52,7 @@ namespace Corvus.Extensions.Cosmos.Internal
         }
 
         /// <inheritdoc/>
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
         {
             if (writer is null)
             {
