32e4e57b88e346c44b3c039e32b6d066cc40a2f9
Mike Larah <mike.larah@endjin.com>
2020-04-16 11:23:13 +0100

    Mark entity as notnull

--- Solutions/Corvus.Extensions.CosmosClient/Corvus/Extensions/Cosmos/IEntityInstance.cs
+++ Solutions/Corvus.Extensions.CosmosClient/Corvus/Extensions/Cosmos/IEntityInstance.cs
@@ -12,7 +12,7 @@ namespace Corvus.Extensions.Cosmos
         /// <summary>
         /// Gets or sets the entity.
         /// </summary>
-        object? Entity { get; set; }
+        object Entity { get; set; }
 
         /// <summary>
         /// Gets or sets the ETag for the entity.
@@ -25,6 +25,7 @@ namespace Corvus.Extensions.Cosmos
     /// </summary>
     /// <typeparam name="T">The type of the entity.</typeparam>
     public interface IEntityInstance<T> : IEntityInstance
+        where T : notnull
     {
         /// <summary>
         /// Gets or sets the entity of the given type.
