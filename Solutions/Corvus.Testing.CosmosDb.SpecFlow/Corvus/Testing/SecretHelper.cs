d9b51a6abfa5110bbfa5cf51389e3bb70f9c3ee2
Ian Griffiths <ian@interact-sw.co.uk>
2021-08-27 14:54:54 +0100

    Upgrade various Corvus libraries
    Use new Corvus.Identity support for legacy connection strings

--- Solutions/Corvus.Testing.CosmosDb.SpecFlow/Corvus/Testing/SecretHelper.cs
+++ Solutions/Corvus.Testing.CosmosDb.SpecFlow/Corvus/Testing/SecretHelper.cs
@@ -5,12 +5,9 @@
 namespace Corvus.Testing
 {
     using System;
-    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
     using Azure;
-    using Azure.Core;
-    using Azure.Identity;
     using Azure.Security.KeyVault.Secrets;
 
     using Microsoft.Extensions.Configuration;
@@ -104,27 +101,7 @@ namespace Corvus.Testing
             }
 
             string azureServicesAuthConnectionString = configuration["AzureServicesAuthConnectionString"];
-
-            // Irritatingly, v12 of the Azure SDK has done away with the AppAuthentication connection
-            // strings that AzureServiceTokenProvider used to support, making it very much harder to
-            // allow an application to switch between different modes of authentication via configuration.
-            // This code supports some of the ones we often use.
-            const string appIdPattern = "RunAs=App;AppId=(?<AppId>[A-Fa-f0-9]{8}(?:-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12});TenantId=(?<TenantId>[A-Fa-f0-9]{8}(?:-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12});AppKey=(?<AppKey>[^;]*)";
-            TokenCredential keyVaultCredentials = (azureServicesAuthConnectionString?.Trim() ?? string.Empty) switch
-            {
-#pragma warning disable SA1122 // Use string.Empty for empty strings - StyleCop analyzer 1.1.118 doesn't understand patterns; it *has* to be "" here
-                "" => new DefaultAzureCredential(),
-#pragma warning restore SA1122 // Use string.Empty for empty strings
-
-                "RunAs=Developer;DeveloperTool=AzureCli" => new AzureCliCredential(),
-                "RunAs=Developer;DeveloperTool=VisualStudio" => new VisualStudioCredential(),
-                "RunAs=App" => new ManagedIdentityCredential(),
-
-                string s when Regex.Match(s, appIdPattern) is Match m && m.Success =>
-                    new ClientSecretCredential(m.Groups["TenantId"].Value, m.Groups["AppId"].Value, m.Groups["AppKey"].Value),
-
-                _ => throw new InvalidOperationException($"AzureServicesAuthConnectionString configuration value '{azureServicesAuthConnectionString}' is not supported in this version of Corvus Tenancy")
-            };
+            var keyVaultCredentials = Corvus.Identity.ClientAuthentication.Azure.LegacyAzureServiceTokenProviderConnectionString.ToTokenCredential(azureServicesAuthConnectionString);
 
             var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
             var keyVaultClient = new SecretClient(keyVaultUri, keyVaultCredentials);
